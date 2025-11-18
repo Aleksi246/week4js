ca4bb4a75d372bbec85e44065c4a7ad69bae7a28
Aleksi <aleklap@metropolia.fi>
2025-11-18 12:42:18 +0200

    first task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import './App.css';
-import Greeting from './components/Greeting';
-import PizzaMenu from './components/PizzaMenu'
-const App = () => {
+import Home from './components/Home';
 
-  const sitename = 'wsk'
-  const styles = {
-    backgroundColor: 'black',
-    color: 'white',
-  }
-  
+const App = () => {
   return (
     <>
-      <h1 style={styles}>My App</h1>
-      <p style = {{color: 'brown'}}>{sitename}</p>
-      <Greeting name={45} age={25} isteacher={false}/>
-      <Greeting name='jaska'age={105} isteacher={true}></Greeting>
-      <PizzaMenu></PizzaMenu>
-
+      <h1>My App</h1>
+      <Home />
     </>
   );
 };
