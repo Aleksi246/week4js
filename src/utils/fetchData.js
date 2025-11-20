4d29537f2effaf77afa755e56f1a50ec058fe948
Aleksi <aleklap@metropolia.fi>
2025-11-20 10:50:01 +0200

    hook1 done

---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export default async function fetchData(url, options = {}) {
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error('fetchData error:', err);
+    throw err;
+  }
+}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,46 +1,25 @@
 //import { useState } from "react";
 import MediaRow from "../components/MediaRow";
 import Single from "../views/Single";
+import { useState, useEffect, useRef } from 'react';
+import fetchData from '../utils/fetchData'
 
-const mediaArray = [
-  {
-    media_id: 8,
-    user_id: 5,
-    filename: "https://placehold.co/1200x800.jpg?text=Pic1",
-    thumbnail: "https://placehold.co/320x240.jpg?text=Pic1thumb",
-    filesize: 170469,
-    media_type: "image/jpeg",
-    title: "Picture 1",
-    description: "This is a placeholder picture.",
-    created_at: "2024-01-07T20:49:34.000Z",
-  },
-  {
-    media_id: 9,
-    user_id: 7,
-    filename: "https://placehold.co/1200x800.jpg?text=Pic2",
-    thumbnail: "https://placehold.co/320x240.jpg?text=Pic2thumb",
-    filesize: 1002912,
-    media_type: "image/jpeg",
-    title: "Pic 2",
-    description: "",
-    created_at: "2024-01-07T21:32:27.000Z",
-  },
-  {
-    media_id: 17,
-    user_id: 2,
-    filename:
-      "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
-    thumbnail: "https://placehold.co/320x240.jpg?text=vid3thumb",
-    filesize: 1236616,
-    media_type: "video/mp4",
-    title: "Bunny",
-    description: "Butterflies fly around the bunny.",
-    created_at: "2024-01-07T20:48:13.000Z",
-  },
-];
+
+
+
+
 
 const Home = () => {
   //const [selectedItem, setSelectedItem] = useState(null);
+  const [mediaArray, setMediaArray] = useState([]);
+
+  useEffect(() => {
+const getMedia = async () => {
+   const json = await fetchData('../public/test.json');
+   setMediaArray(json);
+};
+getMedia();
+},[]);
 
   return (
     <>
